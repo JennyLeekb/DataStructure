48da6b2de943f598110601046d7926320d6f81b1
lkb <cuglkb@163.com>
2018-09-19 15:28:10 +0800

    2018-9-19 堆排序

--- 排序/C语言/sort/main.c
+++ 排序/C语言/sort/main.c
@@ -26,12 +26,21 @@ int main()
     for(i=0;i<n;i++)
         printf("%d  ",test[i].key);*/
 
-    DataType test[12] = {65,34,25,87,12,38,56,46,14,77,92,23};
+    /*DataType test[12] = {65,34,25,87,12,38,56,46,14,77,92,23};
     int n = 12,i;
 
     selectSort(test,n);
 
     printf("直接选择排序的结果为：\n");
+    for(i=0;i<n;i++)
+        printf("%d  ",test[i].key);*/
+
+    DataType test[8] = {{10},{50},{32},{5},{76},{9},{40},{88} };
+    int n = 8,i;
+
+    heapSort(test,n);
+
+    printf("堆排序的结果：\n");
     for(i=0;i<n;i++)
         printf("%d  ",test[i].key);
 
--- 排序/C语言/sort/sort.h
+++ 排序/C语言/sort/sort.h
@@ -10,6 +10,7 @@ typedef struct
 void insertSort(DataType a[], int n);
 void shellSort(DataType a[], int n, int d[], int numOfD);
 void selectSort(DataType a[],int n);
+void heapSort(DataType a[], int n);
 
 
 //直接排序
@@ -91,10 +92,70 @@ void selectSort(DataType a[],int n)
             a[i] = minData;
             a[index] = temp;
         }
-
     }
 
 }
+
+
+//调整非叶子节点h使得a[]满足最大堆，n为数组a中的元素个数
+void createHeap(DataType a[], int n, int h)
+{
+   int i,j,flag;
+   DataType temp;
+
+   i = h;  //i为要建堆的二叉树的根结点
+   j = 2*i+1;
+   temp = a[i];
+   flag = 0;
+
+   while(j < n && flag != 1)
+   {
+       if(j<n-1 && a[j].key < a[j+1].key) j++;
+
+       if(temp.key > a[j].key)
+       {
+           flag = 1;
+       }
+       else
+       {
+           a[i] = a[j];
+           i = j;
+           j = 2*i+1;
+       }
+   }
+
+   a[i] = temp;
+
+}
+
+//把数组元素a[0]~a[n-1]初始化创建为最大堆
+void initCreateHeap(DataType a[], int n)
+{
+    int i;
+
+    for(i=(n-2)/2 ; i>=0; i--)
+        createHeap(a,n,i);
+}
+
+//堆排序
+void heapSort(DataType a[], int n)
+{
+    int i;
+    DataType temp;
+
+    initCreateHeap(a,n);
+
+    for(i=n-1;i>0;i--)  //当前堆的个数每次递减1
+    {
+        temp = a[0];    //交换当前根结点和最后一个结点
+        a[0] = a[i];
+        a[i] = temp;
+
+        createHeap(a,i,0); //重新调整根结点，使之满足最大堆
+    }
+}
+
+
 
 
 #endif // __SORT_H__
